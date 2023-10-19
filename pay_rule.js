13251fd9dc2e00eeea5bd5702beaab71c744ef9f
Harmony Chofor <jethroharmony11@gmail.com>
2023-10-19 11:02:10 -0500

    first commit

---
+++ pay_rule.js
@@ -0,0 +1,20 @@
+
+//INPUT PARAMENTERS
+const payRate =17.30
+const hoursworked =45
+
+// OUTPUT RETURNED
+let pay = 0
+
+//IS REGULAR?
+if(hoursworked <= 40){
+    pay = payrate * hoursworked;
+}else{
+    standardpay =payRate * 40;
+    overtimeHours = hoursWorked - 40
+    overtimeRate = payRate * 1.5
+    overTimePay = overtimeHours * overtimeRatepay
+    pay = sandardpay + overTimePay
+}
+
+console.log(pay)
